c63a95a431ba784e3665b8912cbf4c912c33a88d
Mohammed Musbahu Abdullahi <greenbasketgloball@gmail.com>
2026-09-25 13:44:43 +0100

    fix: clean owner root entry

--- app/owner/page.tsx
+++ app/owner/page.tsx
@@ -1,6 +1,6 @@
-import { redirect } from "next/navigation";
 import { getOwnerSession } from "@/lib/auth";
 import OwnerLogin from "./login/page";
+import OwnerOverview from "./overview/page";
 
 export default async function OwnerEntry() {
   const ownerSession = await getOwnerSession();
@@ -10,9 +10,4 @@ export default async function OwnerEntry() {
   }
 
   return <OwnerOverview />;
-}
-
-async function OwnerOverview() {
-  const { default: Overview } = await import("./overview/page");
-  return <Overview />;
 }
